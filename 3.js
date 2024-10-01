b3677bc94a699cc6ad0de0bf760a1469426e01e0
Élaff Ramos <valelaff10@gmail.com>
2024-10-01 15:17:41 -0300

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,30 @@
+let africa = document.querySelector('.africa')
+let asia = document.querySelector('.asia')
+let america = document.querySelector('.america')
+let europa = document.querySelector('.europa')
+let oceania = document.querySelector('.oceania')
+let res = document.querySelector('.res')
+
+function continente(){
+    if(africa.checked) {
+        res.innerHTML = 'Resposta errada, tente novamente!'
+    } else if (asia.checked) {
+        res.innerHTML = 'Resposta certa!'
+    } else if (america.checked) {
+        res.innerHTML = 'Resposta errada, tente novamente!'
+    } else if (europa.checked) {
+        res.innerHTML = 'Resposta errada, tente novamente!'
+    } else if (oceania.checked) {
+        res.innerHTML = 'Resposta errada, tente novamente!'
+    } else {
+        alert('[ERRO] Selecione uma das opções acima!')
+    }
+}
+
+function proximo() {
+    if(asia.checked){
+        window.location = '4.html'
+    } else {
+        alert('[ERRO] Acerte primeiro a questão!')
+    }
+}
